ac70c6d1a9cc3ba6549b3d05400c94d12aecde88
Shoabe Shariff <shoabe.shariff@gmail.com>
2017-08-08 22:50:27 -0700

    Made stepdefinitions more clear
    [#150073594](https://www.pivotaltracker.com/story/show/150073594)

--- src/test/acceptance/features/step-definitions/stepdefinitions.js
+++ src/test/acceptance/features/step-definitions/stepdefinitions.js
@@ -5,12 +5,14 @@ module.exports = function () {
     browser.url(url);
   });
 
-  this.Then(/^I see the label "([^"]*)" for (.*)$/, (label, elementSelector) => {
-    expect(browser.getText('#' + elementSelector + '-label')).to.be.eql(label);
+  this.Then(/^I see the label "([^"]*)" for (.*)$/, (expectedLabel, elementSelector) => {
+    var actuallabel = browser.getText('#' + elementSelector + '-label');
+    expect(actuallabel).to.be.eql(expectedLabel);
   });
 
   this.Then(/^I see a value equal to the (.*)$/, (field) => {
-    expect(browser.getText('#' + field + '-value')).to.match(/^[0-9]*.[0-9]{2}$/)
+    var valueOfField = browser.getText('#' + field + '-value');
+    expect(valueOfField).to.match(/^[0-9]*.[0-9]{2}$/, "Value for " + field + " is not a number")
   });
 
 };
